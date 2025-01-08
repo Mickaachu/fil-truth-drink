141dd039d560fc7a3f4745b2b5a092f0f4a4b816
Erin <erin@3xpo.com>
2025-01-08 15:17:06 +0800

    Enhance homepage layout and styling for improved user experience

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Link href="/truth-or-drink">
-        Truth or Drink
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+      <h1 className="text-3xl font-bold mb-4">Welcome to Truth or Drink</h1>
+      <Link href="/truth-or-drink ">
+        <div className="bg-blue-500 text-white px-6 py-2 rounded-lg shadow-md hover:bg-blue-600">
+        Start Playing
+        </div>
       </Link>
     </div>
   );
